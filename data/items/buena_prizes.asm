62878b0f2756fb4e251c3bc7619997a2dcb78e1b
JustinScott2 <130512614+JustinScott2@users.noreply.github.com>
2026-03-27 16:17:23 -0700

    Update buena_prizes.asm
    added new items to buena

--- data/items/buena_prizes.asm
+++ data/items/buena_prizes.asm
@@ -4,10 +4,9 @@ BlueCardMartData:
 	db EJECT_PACK,     1
 	db RED_CARD,       1
 	db MINT_LEAF,      2
-	db ABILITY_PATCH,  5
-	db ABILITY_CAP,    5
-	db PUNCHING_GLOVE, 5
-	db RARE_CANDY,     5
+	db PUNCHINGLOVE,   5
 	db EVIOLITE,       5
-
+	db ABILITY_CAP,    5
+	db ABILITYPATCH,   5
+	db RARE_CANDY,     5
 	db -1
